62f8725c8e6cb94282fb91f85bdfd714b033925a
your name here <johnobrien8642@gmail.com>
2020-10-15 14:23:25 -0400

    ::SQLObject::columns created and passed testing

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -5,18 +5,28 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        * 
+      FROM 
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+    cols.map!(&:to_sym)
+    @columns = cols
   end
 
   def self.finalize!
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.name.underscore.pluralize
   end
 
   def self.all
@@ -32,7 +42,14 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, val|
+      attr_name = attr_name.to_sym
+      if self.class.columns.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "unknown attribute '#{attr_name}"
+      end
+    end
   end
 
   def attributes
