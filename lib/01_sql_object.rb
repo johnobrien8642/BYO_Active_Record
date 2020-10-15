73d21f21ac44795791cfea73fafe8804eba89207
your name here <johnobrien8642@gmail.com>
2020-10-15 15:50:16 -0400

    ::SQLObject#insert created and passed testing

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -100,7 +100,17 @@ class SQLObject
   end
 
   def update
-    # ...
+    values_to_insert = self.class_columns
+      .map { |attr| "#{attr} = ?" }.join(", ")
+    
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        #{self.class.table_name}.id = ?
+    SQL
   end
 
   def save
